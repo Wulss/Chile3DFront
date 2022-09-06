1faeaf80fa79970d4705e622cf91a05076302a5f
Wuls <juanpablo.lv@live.cl>
2022-09-05 21:47:16 -0300

    Get files and choose one to download

--- src/components/SearchMap/SearchMap.jsx
+++ src/components/SearchMap/SearchMap.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import { ReactDOM } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import OlMap from "ol/map";
 import OlView from "ol/view";
 import OlLayerTile from "ol/layer/tile";
@@ -12,116 +12,159 @@ import OlLayerVector from "ol/layer/vector";
 import OlStyle from "ol/style";
 import OlProj from "ol/proj";
 import { useEffect } from "react";
+import { List } from "@material-ui/icons";
 
 export function SearchMap() {
-    return (
-        <Map>
-        </Map>
-    );
+  return <Map></Map>;
 }
 
 class Map extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = { center: [0, 0], zoom: 2 };
-    
-        this.olmap = new OlMap({
-          target: null,
-          layers: [
-            new OlLayerTile({
-              source: new OlSourceOSM()
-            })
-          ],
-          view: new OlView({
-            center: this.state.center,
-            zoom: this.state.zoom
-          })
-        });
-        
-        this.source = new OlSourceVector({ wrapX: false });
-      }
+  constructor(props) {
+    super(props);
+    this.state = { center: [-412304.01680521877, 4926693.014295412], zoom: 12 };
 
-    
-      updateMap() {
-        this.olmap.getView().setCenter(this.state.center);
-        this.olmap.getView().setZoom(this.state.zoom);
-      }
-    
-      componentDidMount() {
-        this.olmap.setTarget("map");
-    
-        this.olmap.on("moveend", () => {
-          let center = this.olmap.getView().getCenter();
-          let zoom = this.olmap.getView().getZoom();
-          this.setState({ center, zoom });
-        });
-      }
-    
-      shouldComponentUpdate(nextProps, nextState) {
-        let center = this.olmap.getView().getCenter();
-        let zoom = this.olmap.getView().getZoom();
-        if (center === nextState.center && zoom === nextState.zoom) return false;
-        return true;
-      }
-    
-      userAction() {
-        this.setState({ center: [-7000000, -4100000], zoom: 5 });
-      }
+    this.olmap = new OlMap({
+      target: null,
+      layers: [
+        new OlLayerTile({
+          source: new OlSourceOSM(),
+        }),
+      ],
+      view: new OlView({
+        center: this.state.center,
+        zoom: this.state.zoom,
+      }),
+    });
 
-      draw() {
-        let draw = new OlDraw({
-            source: this.source,
-            type: "Polygon"
+    this.source = new OlSourceVector({ wrapX: false });
+  }
+
+  updateMap() {
+    this.olmap.getView().setCenter(this.state.center);
+    this.olmap.getView().setZoom(this.state.zoom);
+  }
+
+  componentDidMount() {
+    this.olmap.setTarget("map");
+
+    this.olmap.on("moveend", () => {
+      let center = this.olmap.getView().getCenter();
+      let zoom = this.olmap.getView().getZoom();
+      this.setState({ center, zoom });
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    let center = this.olmap.getView().getCenter();
+    let zoom = this.olmap.getView().getZoom();
+    if (center === nextState.center && zoom === nextState.zoom) return false;
+    return true;
+  }
+
+  userAction() {
+    this.setState({ center: [-7000000, -4100000], zoom: 5 });
+  }
+  
+
+  draw() {
+    let draw = new OlDraw({
+      source: this.source,
+      type: "Polygon",
+    });
+    this.olmap.addInteraction(draw);
+    draw.on("drawend", (e) => {
+      var polygon = e.feature.getGeometry();
+      var coords = polygon.getCoordinates();
+      for (var i = 0; i < coords[0].length; i++) {
+        coords[0][i] = OlProj.transform(coords[0][i], "EPSG:3857", "EPSG:4326");
+      }
+      var geojson = {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "Polygon",
+              coordinates: coords,
+            },
+          },
+        ],
+      };
+      geojson = JSON.stringify(geojson);
+      
+      
+      var makeRequest = function () {
+        fetch(
+          "https://6a23-2800-150-14b-227c-a88b-77cd-6ff4-d847.ngrok.io/geojson  ",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+            body: geojson,
+          }
+        ).then((response) => {
+          if (response.ok) {
+            response.json().then((json) => {
+              console.log(json);
+              var archive = String(json["inside"][0]);
+              console.log(archive);
+
+              var archivo = {
+                files: [archive],
+              };
+              var data = JSON.stringify(archivo);
+              console.log(data);
+              var downloadData = function () {
+                fetch(
+                  "https://6a23-2800-150-14b-227c-a88b-77cd-6ff4-d847.ngrok.io/files",
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                      "Access-Control-Allow-Origin": "*",
+                      "Accept": "*/*",
+                      "Accept-Encoding": "gzip, deflate, br",
+                      "Connection": "keep-alive",
+                    }, 
+                    body: {
+                      data
+                  },
+                  }
+                ).then((response) => {
+                  console.log("tengo respuestsa");
+                  if(response.ok) {
+                   response.json().then((json) => {
+                      console.log("respuesta ok");
+                    
+                      console.log(json);
+                   })
+                  }
+                });
+              }
+              downloadData();
             });
-            this.olmap.addInteraction(draw);
-            draw.on("drawend", e => {
-                var polygon = e.feature.getGeometry();
-                var coords = polygon.getCoordinates();
-                for (var i = 0; i < coords[0].length; i++) {
-                    coords[0][i] = OlProj.transform(coords[0][i], "EPSG:3857", "EPSG:4326");
-                }
-                var geojson = {
-                    type: "FeatureCollection",
-                    features: [
-                        {
-                            type: "Feature",
-                            properties: {},
-                            geometry: {
-                                type: "Polygon",
-                                coordinates: coords}}
-                    ]
-                };
-                geojson = JSON.stringify(geojson);
+          }
+        });
+      };
+      makeRequest();
 
-                useEffect (() => {
-                    fetch("https://google.com", {
-                        method: "GET",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: geojson
-                    })
-                }, []);
+      
+      console.log(geojson);
+    });
+  }
 
-
-                console.log(geojson);
-
-
-            });
-        }
-
-    
-      render() {
-        return (
-          <div id="map" style={{ width: "100%", height: "60vh" }}>
-            <button onClick={e => this.userAction()}>goto Chile</button>
-            <button onClick={e => this.draw()}>Draw</button>
-          </div>
-        );
-      }
-    
-    }
+  render() {
+    return (
+      <div id="map" style={{ width: "100%", height: "60vh" }}>
+        <button onClick={(e) => this.userAction()}>goto Chile</button>
+        <button onClick={(e) => this.draw()}>Draw</button>
+      </div>
+    );
+  }
+}
 
 /* class Polygon extends Component {
   componentDidMount() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {StrictMode} from 'react';
+
+// ✅ now importing from react-dom/client
+import {createRoot} from 'react-dom/client';
+
 import { App} from './App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// 👇️ IMPORTANT: make sure to specify correct ID
+// must be the ID of the div element in your index.html file
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
